0c6aff4f350e3bb70a75759d4707726a9ec46d04
Asko Soukka <asko.soukka@iki.fi>
2025-04-08 20:25:46 +0300

    librkllmrt: 1.2.0

--- devenv/modules/librkllmrt.nix
+++ devenv/modules/librkllmrt.nix
@@ -4,10 +4,10 @@
   autoPatchelfHook,
 }:
 stdenv.mkDerivation {
-  name = "librkllmrt-1.1.4";
+  name = "librkllmrt-1.2.0";
   src = builtins.fetchurl {
-    url = "https://github.com/airockchip/rknn-llm/raw/8623edd0559a07e7127876d685f2b7ca8b83590c/rkllm-runtime/Linux/librkllm_api/aarch64/librkllmrt.so";
-    sha256 = "sha256:02nd7dy3nljgj0y66ifq6g3gmllqhk73z5n4k4gx7gy30lqkbvrw";
+    url = "https://github.com/airockchip/rknn-llm/raw/refs/heads/main/rkllm-runtime/Linux/librkllm_api/aarch64/librkllmrt.so";
+    sha256 = "sha256:1sh0n16dxl0f0vr91psl6mwngdxxkk7hygdsprq1cj42rl3z28sa";
   };
   nativeBuildInputs = [
     libgcc
